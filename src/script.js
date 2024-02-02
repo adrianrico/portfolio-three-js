cdc497f2453d70f823878808d7473652a6701fdd
adrianrico <adrianricoitm@gmail.com>
2024-02-02 12:36:14 -0600

    Updated section 0 - added UI elements

--- src/script.js
+++ src/script.js
@@ -5,33 +5,53 @@ import { NearestFilter } from 'three'
 import gsap from 'gsap'
 
 
-//#region Debug
-const gui = new dat.GUI()
-
+//#region global variables
 const parameters = { materialColor: '#00c8ff'}
+//#endregion global variables
 
-gui
+
+
+
+
+//#region Debug
+/**
+ * Add the color change control to the UI
+ * 
+ * NOTES
+ * To be removed to reduce loading time...
+ * To be tested...
+ */
+
+//const gui = new dat.GUI()
+
+/* gui
     .addColor(parameters, 'materialColor')
     .onChange(()=>
     {
         material.color.set(parameters.materialColor)
         particlesMaterial.color.set(parameters.materialColor)
-    })
+    }) */
 //#endregion Debug
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
 
-// Scene
-const scene = new THREE.Scene()
 
+
+
+//#region 3d objects
 /**
- * Using individual objects
- * Create one material and apply to all objects
+ * Add three.js elements to the page
+ * 
+ * NOTES:
+ * Using individual objects...
+ * Create one material and apply to all objects...
  */
+
+const canvas    = document.querySelector('canvas.webgl') //-->Fetch the canvas from the HTML...
+const scene     = new THREE.Scene()
+
 const textureLoader         = new THREE.TextureLoader()
 const gradientTexture       = textureLoader.load('textures/gradients/5.jpg')
-gradientTexture.magFilter   = NearestFilter
+gradientTexture.magFilter   = NearestFilter //--> Added to improve visualization of the elements...
 
 const material = new THREE.MeshToonMaterial({
     color:          parameters.materialColor,
@@ -57,8 +77,6 @@ const mesh3 = new THREE.Mesh(
     material
 )
 
-
-
 mesh1.position.y = -objectsDistance * 0
 mesh2.position.y = -objectsDistance * 1
 mesh3.position.y = -objectsDistance * 2
@@ -69,21 +87,16 @@ mesh3.position.x =  3
 
 mesh1.position.z =  0.5
 
-
-
 scene.add(mesh1 , mesh2, mesh3)
 
-const sectionMeshes = [mesh1, mesh2, mesh3,mesh3,mesh3]
+const sectionMeshes = [mesh1, mesh2, mesh3, mesh3, mesh3]
 
-/**8
- * PARTICLES
- */
-const particlesCount = 200
-const positions = new Float32Array(particlesCount * 3)
+/** #8 PARTICLES */
+const particlesCount    = 200
+const positions         = new Float32Array(particlesCount * 3)
 
 for (let i = 0; i < particlesCount; i++) 
 {
-   
     positions[i * 3 + 0] = (Math.random() -0.5) * 10 
     positions[i * 3 + 1] =  objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length 
     positions[i * 3 + 2] = (Math.random() -0.5) * 10
@@ -101,12 +114,21 @@ const particlesMaterial = new THREE.PointsMaterial({
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
 
-/**REQUIRED FOR TOON MATERIAL */
+/** Required for toon material */
 const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
 directionalLight.position.set(1,1,0)
 scene.add(directionalLight)
 
-// Sizes
+//#endregion 3d objects
+
+
+
+
+
+//#region screen rezising
+/**
+ * Dinamically change the size of the screen
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -114,18 +136,19 @@ const sizes = {
 
 window.addEventListener('resize', () =>
 {
-    // Update sizes
+    /** Update sizes */
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
-    // Update camera
+    /** Update camera */
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
-    // Update renderer
+    /** Update renderer */
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
+//#endregion screen rezising
 
 
 /** Create a group to fix moving camera with the mouse move issue */
@@ -133,7 +156,7 @@ const cameraGroup = new THREE.Group()
 scene.add(cameraGroup)
 
 
-// Base camera
+/** Base camera */ 
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
 cameraGroup.add(camera)
@@ -150,25 +173,28 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**4 SCROLLING CAMERA ENABLE */
 /**9 TRIGGERED ANIMATION */
-let scrollY = window.scrollY
-let currentSection = 0
+let scrollY         = window.scrollY
+let currentSection  = 0
 
-let executed = false
+let executed    = false
+let executed2   = false 
 
-const descriptionText = 'I\'m an informatics engineer and i\'m currently working for the hot steel rolling industry in Mexico'+
+const descriptionText = 'I\'m an informatics engineer and I\'m currently working for the hot steel rolling industry in Mexico'+
 ' as a process automation engineer also known as a level 2 engineer. By applying my skills and learning'+
-' new ones i\'m always looking to contribute and improve the level 2 automation.'+
+' new ones I\'m always looking to contribute and improve the level 2 automation.'+
 '<br><br>'+
-'As a process automation engineer i\'m able to analyze field signals from the process, analyze the steel '+ 
+'As a process automation engineer I\'m able to analyze field signals from the process, analyze the steel '+ 
 'cooling process, make a diagnostic and understand step by step all the possible factors involved '+ 
 'in the process.'+
 '<br><br>'+
-'By using my programming skills i\'m able to make my own c++ prototypes to test and improve the steel cooling control process.'+
+'By using my programming skills I\'m able to make my own C++ prototypes to test and improve the steel cooling control process.'+
 '<br><br>'+
-'Also i have been able to apply my web development skills to create web portals that improves process and '+ 
-'administrative activities using different techniques like AJAX, JQUERY, BOOTSTRAP, etc.'+ 
-'Right now i\'m improving my skills on Three.JS and Javascript in order to create our own tools using '+ 
+'Also I have been able to apply my web development skills to create web portals that improves process and '+ 
+'administrative activities using different techniques like AJAX, JQUERY, BOOTSTRAP, etc. '+ 
+'Right now I\'m improving my skills on Three.JS and Javascript in order to create our own tools using '+ 
 'web developments and add now a 3D functionality to web developments as the background shapes you see.'
+
+const projectTypingDescription = 'A brief showcase of some projects I have been working on and can give you an idea of what I can do for you.'
 
 var typingOptions = {
     strings: [descriptionText],
@@ -182,7 +208,7 @@ window.addEventListener('scroll',() =>
     scrollY = window.scrollY
 
     const newSection = Math.round(scrollY / sizes.height)
-    //console.log("NEW: ",newSection,"CURRENT: ",currentSection)
+    console.log("NEW: ",newSection,"CURRENT: ",currentSection)
 
     if (newSection != currentSection) 
     {
@@ -198,11 +224,30 @@ window.addEventListener('scroll',() =>
             }
         )    
 
+
+        var typed
         if (currentSection == 1 && executed == false)
         {
-            var typed = new Typed('.typing_anim2', typingOptions)
+            typingOptions.strings = [descriptionText]
+            typed = new Typed('.typing_anim2', typingOptions)
             executed = true
         }
+        
+        if ( currentSection == 2 && executed2 == false) 
+        {
+            typingOptions.strings = [projectTypingDescription]
+            typed = new Typed('.typing_anim3', typingOptions)
+            
+            typingOptions.strings = ["C++ algorithm that process temperatures signals from field measurements and determines if there might "+
+            "be external factors involved like water or strip surface defects that cause noise into the automation process."]
+            typed = new Typed('.typing_anim4', typingOptions)
+
+            executed2 = true
+        }
+         
+        
+       
+        
         
         
     }
